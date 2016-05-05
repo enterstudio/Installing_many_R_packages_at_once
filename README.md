0037a2a51726e88df4ff17b26ae99d99eed8a7ef
fissehab <fishjhu@gmail.com>
2016-05-05 13:27:24 -0400

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Installing_many_R_packages_at_once
+### Installing many R packages at once
+
+This script helps to install many R packages at once. You can add or modify the packages you want and run it. 
+
+You can read more from my website [here](http://datascience-enthusiast.com/R/install_load_many_packages_R.html).
---
+++ install_many_R_packages_at_once.r
@@ -0,0 +1,76 @@
+
+my_packages=c(
+  
+"bitops",
+"calibrate",
+"compare",
+"caret",
+"caTools",
+"chron",
+"data.table",
+"dbscan",
+"digest",
+"downloader",
+"DT",
+"e1071",
+"evaluate",
+"fields",
+"formatR",
+"ggmap",
+"ggplot2",
+"ggthemes",
+"gridExtra",
+"gtrendsR",
+"highr",
+"htmltools",
+"httr",
+"jsonlite",
+"lattice",
+"lubridate",
+"manipulate",
+"maps",
+"maptools",
+"markdown",
+"NeuralNetTools",
+"nnet",
+"pdftools",
+"plotmo",
+"plotly",
+"plyr",
+"randomForest",
+"RColorBrewer",
+"RCurl",
+"readr",
+"reshape2",
+"rgdal",
+"rmarkdown",
+"ROAuth",
+"ROCR",
+"rpart",
+"rvest",
+"scales",
+"shiny",
+"shinydashboard",
+"sp",
+"sqldf",
+"stringi",
+"stringr",
+"tidyr",
+"tm",
+"twitteR",
+"wordcloud",
+"xlsx",
+"XML",
+"yaml"
+    
+)
+
+to_install= function(packages){
+   not_yet_installed <- packages[!(packages %in% installed.packages()[, "Package"])]
+    
+    if (length(to_install)){
+        install.packages(not_yet_installed, repos='http://cran.us.r-project.org',dependencies = TRUE)
+              }
+  }
+
+to_install(my_packages)
